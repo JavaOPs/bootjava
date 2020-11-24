ddba8635b0aa9726287de64ab350dfad59472dfc
admin@javaops.ru <gitlaB13>
2020-11-24 16:51:36 +0300

    3_01_jpa_data_rest

--- src/main/java/ru/javaops/bootjava/repository/UserRepository.java
+++ src/main/java/ru/javaops/bootjava/repository/UserRepository.java
@@ -2,6 +2,7 @@ package ru.javaops.bootjava.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javaops.bootjava.model.User;
 
@@ -11,8 +12,10 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @RestResource(rel = "by-email", path = "by-email")
     @Query("SELECT u FROM User u WHERE u.email = LOWER(:email)")
     Optional<User> findByEmailIgnoreCase(String email);
 
+    @RestResource(rel = "by-lastname", path = "by-lastname")
     List<User> findByLastNameContainingIgnoreCase(String lastName);
 }
