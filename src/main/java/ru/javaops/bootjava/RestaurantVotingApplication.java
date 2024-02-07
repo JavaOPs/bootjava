c5c6ad6eb7e01279747b16b72a152b16928cb880
Andrey Belov <belovandr96@gmail.com>
2024-02-07 11:18:52 +0500

    add sout hello world
    (cherry picked from commit 100e233c830b6a30f59da4341c9adeac43ede068)

--- src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -10,5 +10,6 @@ public class RestaurantVotingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantVotingApplication.class, args);
+        System.out.println("Hello world!");
     }
 }
