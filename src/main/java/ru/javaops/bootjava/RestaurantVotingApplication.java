79a447731fba45ec1d3624cc7b3c8b09cb0d7846
Andrey Belov <belovandr96@gmail.com>
2024-02-08 13:21:53 +0500

    First commit wins

--- src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -13,5 +13,6 @@ public class RestaurantVotingApplication {
         System.out.println("Hello world!");
         System.out.println("Hello world 2!");
         System.out.println("Hello world 3!");
+        System.out.println("Этот коммит точно будет лучшим");
     }
 }
--- src/main/java/ru/javaops/bootjava/TestClass.java
+++ src/main/java/ru/javaops/bootjava/TestClass.java
@@ -5,4 +5,6 @@ public class TestClass {
     private String name;
     private String surname;
 
+    private String fullname;
+
 }
